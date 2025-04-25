21dc3ace1ac6ed6308e26d3a10294fca26638cfe
hardhiky <hardhikyd9@gmail.com>
2025-04-25 21:19:18 +0530

    fix: update theme import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import { GlobalStyles, theme } from './styles/GlobalStyles';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { theme } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
